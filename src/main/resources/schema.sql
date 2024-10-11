9ecd12c3c9fd6b1abbb02878e0594b72bff028e6
Artur Aronov <unionco@icloud.com>
2024-10-11 13:31:03 +0800

    updated schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,13 +1,13 @@
 CREATE TABLE IF NOT EXISTS Card (
-    id              INT             NOT NULL,
+    id              INT             NOT NULL AUTO_INCREMENT,
     correct         INT             DEFAULT 0,
     wrong           INT             DEFAULT 0,
     skipped         INT             DEFAULT 0,
     question        VARCHAR(250)    NOT NULL,
     answer          VARCHAR(250)    NOT NULL,
-    category        VARCHAR(5)      NOT NULL,
-    date_created    TIMESTAMP       NOT NULL,
-    date_edited     TIMESTAMP       NOT NULL,
+    category        VARCHAR(6)      NOT NULL,
+    date_created    TIMESTAMP       DEFAULT CURRENT_TIMESTAMP,
+    date_edited     TIMESTAMP       DEFAULT NULL,
 
     PRIMARY KEY (id)
 );
