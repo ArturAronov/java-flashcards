a05724b169845ec20d592035384dfe2839976aaa
Artur Aronov <unionco@icloud.com>
2024-10-11 13:30:44 +0800

    added data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO Card (question, answer, category) VALUES ('Question 1', 'Answer 1', 'EASY');
+INSERT INTO Card (question, answer, category) VALUES ('Question 2', 'Answer 2', 'MEDIUM');
+INSERT INTO Card (question, answer, category) VALUES ('Question 3', 'Answer 3', 'HARD');
